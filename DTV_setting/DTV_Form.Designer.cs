5409cbab7456d91b7178c73b9bc178b7582c7aba
PTC <thqv74@gmail.com>
2021-03-23 17:32:19 -0300

    Firts commit

---
+++ DTV_setting/DTV_Form.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace DTV_setting
+{
+    partial class DTV_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btSetDTV = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFrequency = new System.Windows.Forms.TextBox();
+            this.txtAmplitude = new System.Windows.Forms.TextBox();
+            this.groupBoxSetting = new System.Windows.Forms.GroupBox();
+            this.groupBoxSCPI = new System.Windows.Forms.GroupBox();
+            this.txtSCPICommands = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btEXMSetDTV = new System.Windows.Forms.Button();
+            this.groupBoxSetting.SuspendLayout();
+            this.groupBoxSCPI.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btSetDTV
+            // 
+            this.btSetDTV.Location = new System.Drawing.Point(12, 323);
+            this.btSetDTV.Name = "btSetDTV";
+            this.btSetDTV.Size = new System.Drawing.Size(177, 45);
+            this.btSetDTV.TabIndex = 0;
+            this.btSetDTV.Text = "SET DTV - ON";
+            this.btSetDTV.UseVisualStyleBackColor = true;
+            this.btSetDTV.Click += new System.EventHandler(this.btSetDTV_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Frequency(MHz):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Amplitude:";
+            // 
+            // txtFrequency
+            // 
+            this.txtFrequency.Location = new System.Drawing.Point(100, 32);
+            this.txtFrequency.Name = "txtFrequency";
+            this.txtFrequency.Size = new System.Drawing.Size(71, 20);
+            this.txtFrequency.TabIndex = 3;
+            this.txtFrequency.Text = "587.143";
+            // 
+            // txtAmplitude
+            // 
+            this.txtAmplitude.Location = new System.Drawing.Point(100, 73);
+            this.txtAmplitude.Name = "txtAmplitude";
+            this.txtAmplitude.Size = new System.Drawing.Size(71, 20);
+            this.txtAmplitude.TabIndex = 4;
+            // 
+            // groupBoxSetting
+            // 
+            this.groupBoxSetting.Controls.Add(this.label1);
+            this.groupBoxSetting.Controls.Add(this.txtAmplitude);
+            this.groupBoxSetting.Controls.Add(this.label2);
+            this.groupBoxSetting.Controls.Add(this.txtFrequency);
+            this.groupBoxSetting.Location = new System.Drawing.Point(18, 12);
+            this.groupBoxSetting.Name = "groupBoxSetting";
+            this.groupBoxSetting.Size = new System.Drawing.Size(232, 118);
+            this.groupBoxSetting.TabIndex = 5;
+            this.groupBoxSetting.TabStop = false;
+            this.groupBoxSetting.Text = "Setting";
+            // 
+            // groupBoxSCPI
+            // 
+            this.groupBoxSCPI.Controls.Add(this.txtSCPICommands);
+            this.groupBoxSCPI.Location = new System.Drawing.Point(19, 137);
+            this.groupBoxSCPI.Name = "groupBoxSCPI";
+            this.groupBoxSCPI.Size = new System.Drawing.Size(485, 134);
+            this.groupBoxSCPI.TabIndex = 6;
+            this.groupBoxSCPI.TabStop = false;
+            this.groupBoxSCPI.Text = "SCPI Commands";
+            // 
+            // txtSCPICommands
+            // 
+            this.txtSCPICommands.AcceptsReturn = true;
+            this.txtSCPICommands.Location = new System.Drawing.Point(8, 19);
+            this.txtSCPICommands.Multiline = true;
+            this.txtSCPICommands.Name = "txtSCPICommands";
+            this.txtSCPICommands.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSCPICommands.Size = new System.Drawing.Size(471, 108);
+            this.txtSCPICommands.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 37);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "CMW500";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(361, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 37);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "EXM";
+            // 
+            // btEXMSetDTV
+            // 
+            this.btEXMSetDTV.Location = new System.Drawing.Point(305, 323);
+            this.btEXMSetDTV.Name = "btEXMSetDTV";
+            this.btEXMSetDTV.Size = new System.Drawing.Size(193, 45);
+            this.btEXMSetDTV.TabIndex = 9;
+            this.btEXMSetDTV.Text = "SET DTV - ON";
+            this.btEXMSetDTV.UseVisualStyleBackColor = true;
+            this.btEXMSetDTV.Click += new System.EventHandler(this.btEXMSetDTV_Click);
+            // 
+            // DTV_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 391);
+            this.Controls.Add(this.btEXMSetDTV);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBoxSCPI);
+            this.Controls.Add(this.groupBoxSetting);
+            this.Controls.Add(this.btSetDTV);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DTV_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DTV Setting";
+            this.Load += new System.EventHandler(this.DTV_setting);
+            this.groupBoxSetting.ResumeLayout(false);
+            this.groupBoxSetting.PerformLayout();
+            this.groupBoxSCPI.ResumeLayout(false);
+            this.groupBoxSCPI.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSetDTV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFrequency;
+        private System.Windows.Forms.TextBox txtAmplitude;
+        private System.Windows.Forms.GroupBox groupBoxSetting;
+        private System.Windows.Forms.GroupBox groupBoxSCPI;
+        private System.Windows.Forms.TextBox txtSCPICommands;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btEXMSetDTV;
+    }
+}
+
---
+++ DTV_setting/DTV_Form.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Ivi.Visa.Interop; // SCPI functions
+using System.Threading;
+
+namespace DTV_setting
+{
+        
+    public partial class DTV_Form : Form
+    {
+        private Ivi.Visa.Interop.FormattedIO488 ioTestSet;
+        public bool bDTVON = false;
+        
+        public DTV_Form()
+        {            
+            InitializeComponent();
+        }
+
+
+        private void DTV_setting(object sender, EventArgs e)
+        {
+            ioTestSet = new FormattedIO488();
+        }
+
+        private void btSetDTV_Click(object sender, EventArgs e)
+        {
+            string strCommand = string.Empty;
+            string strReturn = string.Empty;
+
+            //CMW500
+            try
+            {
+                ResourceManager grm = new ResourceManager();
+                ioTestSet.IO = (IMessage)grm.Open("RS_CMW500", AccessMode.NO_LOCK, 2000, "");             
+            }
+            catch
+            {
+                ioTestSet.IO = null;                
+            }
+
+
+            if ((ioTestSet != null) && (!bDTVON))
+            {
+                this.txtSCPICommands.Clear();
+
+                if ((txtAmplitude.Text == "") || (Convert.ToDouble(txtAmplitude.Text) > - 10))
+                {
+                    MessageBox.Show("Ajuste a amplitude entre -10 a -65 dBm");
+                    return;
+                }
+
+                if ((txtFrequency.Text == "") || (Convert.ToDouble(txtFrequency.Text) > 803.143) || (Convert.ToDouble(txtFrequency.Text) < 473.143))
+                {
+                    MessageBox.Show("Ajuste a Frequencia entre 473.143 a 803.143 MHz");
+                    return;
+                }
+                
+                this.txtSCPICommands.Text = "-> *IDN? \r\n";
+                ioTestSet.WriteString("*IDN?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                //Verify if DTV wave form can be found                
+                this.txtSCPICommands.Text += "-> MMEM:CAT? 'D:\\Rohde-Schwarz\\CMW\\Data\\waveform\\'" + "\r\n";
+                ioTestSet.WriteString("MMEM:CAT? 'D:\\Rohde-Schwarz\\CMW\\Data\\waveform\\'", true);
+                strReturn = ioTestSet.ReadString();
+                this.txtSCPICommands.Text += "<-" + strReturn + "\r\n";
+
+                if (strReturn.Contains("ISDB-Tb_Digital_TV_withPR.wv") == false)
+                {
+                    MessageBox.Show("Wave Form: \r\n D:\\Rohde-Schwarz\\CMW\\Data\\waveform\\ISDB-Tb_Digital_TV_withPR.wv \r\n Não encontrada !!!","Wave Form Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    return;
+                }
+
+                this.txtSCPICommands.Text += "-> SOUR:GPRF:GEN:ARB:FILE 'D:\\Rohde-Schwarz\\CMW\\Data\\waveform\\ISDB-Tb_Digital_TV_withPR.wv';*OPC? \r\n";
+                ioTestSet.WriteString("SOUR:GPRF:GEN:ARB:FILE 'D:\\Rohde-Schwarz\\CMW\\Data\\waveform\\ISDB-Tb_Digital_TV_withPR.wv';*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                this.txtSCPICommands.Text += "-> " + "SOUR:GPRF:GEN:ARB:FILE?" + "\r\n";
+                ioTestSet.WriteString("SOUR:GPRF:GEN:ARB:FILE?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                this.txtSCPICommands.Text += "-> " + "SOUR:GPRF:GEN:BBM ARB;:SOUR:GPRF:GEN:ARB:REP CONT;:SOUR:GPRF:GEN:LIST OFF;:TRIG:GPRF:GEN:ARB:RETR ON;:TRIG:GPRF:GEN:ARB:AUT ON;*OPC?" + "\r\n";
+                ioTestSet.WriteString("SOUR:GPRF:GEN:BBM ARB;:SOUR:GPRF:GEN:ARB:REP CONT;:SOUR:GPRF:GEN:LIST OFF;:TRIG:GPRF:GEN:ARB:RETR ON;:TRIG:GPRF:GEN:ARB:AUT ON;*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                this.txtSCPICommands.Text += "-> " + "SYST:ERR?" + "\r\n";
+                ioTestSet.WriteString("SYST:ERR?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                this.txtSCPICommands.Text += "-> " + "*CLS" + "\r\n";
+                ioTestSet.WriteString("*CLS", true);
+
+                this.txtSCPICommands.Text += "-> " + "CONFigure:FDCorrection:USAGe? RF1C" + "\r\n";
+                ioTestSet.WriteString("CONFigure:FDCorrection:USAGe? RF1C", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                //Set frequency
+                strCommand = "SOUR:GPRF:GEN1:RFS:FREQ " + txtFrequency.Text.ToString() + " MHz;*OPC?";
+                this.txtSCPICommands.Text += "-> " + strCommand + "\r\n";
+                ioTestSet.WriteString(strCommand, true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                this.txtSCPICommands.Text += "-> " + "SOUR:GPRF:GEN1:STAT ON;*OPC?" + "\r\n";
+                ioTestSet.WriteString("SOUR:GPRF:GEN1:STAT ON;*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                do
+                {
+                    this.txtSCPICommands.Text += "-> " + "SOUR:GPRF:GEN1:STAT?" + "\r\n";
+                    ioTestSet.WriteString("SOUR:GPRF:GEN1:STAT?", true);
+                    strReturn = ioTestSet.ReadString();
+                    this.txtSCPICommands.Text += "<-" + strReturn + "\r\n";
+                }
+                while (strReturn.Contains("OFF") == true);
+                	
+                strCommand = "SOUR:GPRF:GEN1:RFS:LEV " + txtAmplitude.Text.ToString() + " dBm;*OPC?";
+                this.txtSCPICommands.Text += "-> " + strCommand + "\r\n";
+                ioTestSet.WriteString(strCommand, true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                bDTVON = true;
+                this.btSetDTV.Text = "SET DTV - OFF";
+            }
+            else
+            {
+                this.txtSCPICommands.Clear();
+                this.txtSCPICommands.Text += "-> " + "SOUR:GPRF:GEN1:STAT OFF;*OPC?" + "\r\n";
+                ioTestSet.WriteString("SOUR:GPRF:GEN1:STAT OFF;*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                bDTVON = false;
+                this.btSetDTV.Text = "SET DTV - ON";
+            }
+        }
+
+        private void btEXMSetDTV_Click(object sender, EventArgs e)
+        {
+            string strCommand = string.Empty;
+            string strReturn = string.Empty;
+
+            //EXM
+            try
+            {
+                ResourceManager grm = new ResourceManager();
+                ioTestSet.IO = (IMessage)grm.Open("AGILENT_EXT", AccessMode.NO_LOCK, 2000, "");
+            }
+            catch
+            {
+                ioTestSet.IO = null;
+            }
+
+
+            if ((ioTestSet != null) && (!bDTVON))
+            {
+                this.txtSCPICommands.Clear();
+
+                if ((txtAmplitude.Text == "") || (Convert.ToDouble(txtAmplitude.Text) > -10))
+                {
+                    MessageBox.Show("Ajuste a amplitude entre -10 a -65 dBm");
+                    return;
+                }
+
+                if ((txtFrequency.Text == "") || (Convert.ToDouble(txtFrequency.Text) > 803.143) || (Convert.ToDouble(txtFrequency.Text) < 473.143))
+                {
+                    MessageBox.Show("Ajuste a Frequencia entre 473.143 a 803.143 MHz");
+                    return;
+                }
+
+                this.txtSCPICommands.Text = "-> *IDN? \r\n";
+                ioTestSet.WriteString("*IDN?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                this.txtSCPICommands.Text += "->" + "FEED:RF:PORT:OUTP RFIO2;*OPC?" + "\r\n";
+                ioTestSet.WriteString("FEED:RF:PORT:OUTP RFIO2;*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "FEED:RF:PORT:INPUT RFIO2;*OPC?" + "\r\n";
+                ioTestSet.WriteString("FEED:RF:PORT:INPUT RFIO2;*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "SOUR:AM:STAT OFF;:SOUR:FM:STAT OFF;:SOUR:PM:STAT OFF;:OUTP:MOD ON;*OPC?" + "\r\n";
+                ioTestSet.WriteString("SOUR:AM:STAT OFF;:SOUR:FM:STAT OFF;:SOUR:PM:STAT OFF;:OUTP:MOD ON;*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+
+
+                this.txtSCPICommands.Text += "-> " + "*CLS" + "\r\n";
+                ioTestSet.WriteString("*CLS", true);
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "SYST:ERR?" + "\r\n";
+                ioTestSet.WriteString("SYST:ERR?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                this.txtSCPICommands.Text += "-> " + "SOUR:RAD:ARB:STAT OFF;*OPC?" + "\r\n";
+                ioTestSet.WriteString("SOUR:RAD:ARB:STAT OFF;*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "SOUR:RAD:ARB:CAT?" + "\r\n";
+                ioTestSet.WriteString("SOUR:RAD:ARB:CAT?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "SOUR:RAD:ARB:WAV 'ISDB-Tb_Digital_TV_HD.wfm';*OPC?" + "\r\n";
+                ioTestSet.WriteString("SOUR:RAD:ARB:WAV 'ISDB-Tb_Digital_TV_HD.wfm';*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "SYST:ERR?" + "\r\n";
+                ioTestSet.WriteString("SYST:ERR?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                this.txtSCPICommands.Text += "-> " + "SOUR:RAD:ARB:TRIG:TYPE CONT" + "\r\n";
+                ioTestSet.WriteString("SOUR:RAD:ARB:TRIG:TYPE CONT", true);
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "SOUR:RAD:ARB:TRIG:TYPE:CONT FREE" + "\r\n";
+                ioTestSet.WriteString("SOUR:RAD:ARB:TRIG:TYPE:CONT FREE", true);
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "SOUR:RAD:ARB:STAT ON;*OPC?" + "\r\n";
+                ioTestSet.WriteString("SOUR:RAD:ARB:STAT ON;*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "SYST:ERR?" + "\r\n";
+                ioTestSet.WriteString("SYST:ERR?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                this.txtSCPICommands.Text += "-> " + "CORR:CSET4:DESC?" + "\r\n";
+                ioTestSet.WriteString("CORR:CSET4:DESC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "CORR:CSET4:DEL;*OPC?" + "\r\n";
+                ioTestSet.WriteString("CORR:CSET4:DEL;*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "CORR:CSET4:DESC 'Rx RF IO2'" + "\r\n";
+                ioTestSet.WriteString("CORR:CSET4:DESC 'Rx RF IO2'", true);
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "CORR:CSET4:STAT ON;*OPC?" + "\r\n";
+                ioTestSet.WriteString("CORR:CSET4:STAT ON;*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "CORR:CSET4:DATA:MERGE 587142857,1.75;*OPC?" + "\r\n";
+                ioTestSet.WriteString("CORR:CSET4:DATA:MERGE 587142857,1.75;*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+
+                this.txtSCPICommands.Text += "-> " + "CORR:CSET4:COMM 'UUT_RF_CONN2_TO_TESTSET_ALT1_RF';*OPC?" + "\r\n";
+                ioTestSet.WriteString("CORR:CSET4:COMM 'UUT_RF_CONN2_TO_TESTSET_ALT1_RF';*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+
+                //Set frequency
+                strCommand = "SOUR:FREQ " + txtFrequency.Text.ToString() + " MHz;*OPC?";
+                this.txtSCPICommands.Text += "-> " + strCommand + "\r\n";
+                ioTestSet.WriteString(strCommand, true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+                this.txtSCPICommands.Text += "-> " + "OUTP ON;*OPC?" + "\r\n";
+                ioTestSet.WriteString("SOUR:GPRF:GEN1:STAT ON;*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+                Thread.Sleep(100);
+
+                do
+                {
+                    this.txtSCPICommands.Text += "-> " + "OUTP?" + "\r\n";
+                    ioTestSet.WriteString("OUTP?", true);
+                    strReturn = ioTestSet.ReadString();
+                    this.txtSCPICommands.Text += "<-" + strReturn + "\r\n";
+                }
+                while (strReturn.Contains("OFF") == true);
+
+                strCommand = "SOUR:POW " + txtAmplitude.Text.ToString() + " dBm;*OPC?";
+                this.txtSCPICommands.Text += "-> " + strCommand + "\r\n";
+                ioTestSet.WriteString(strCommand, true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                bDTVON = true;
+                this.btEXMSetDTV.Text = "SET DTV - OFF";
+            }
+            else
+            {
+                this.txtSCPICommands.Clear();
+                this.txtSCPICommands.Text += "-> " + "OUTP OFF;*OPC?" + "\r\n";
+                ioTestSet.WriteString("OUTP OFF;*OPC?", true);
+                this.txtSCPICommands.Text += "<-" + ioTestSet.ReadString() + "\r\n";
+
+                bDTVON = false;
+                this.btEXMSetDTV.Text = "SET DTV - ON";
+            }
+        }
+
+        
+     
+    }
+
+     
+}
